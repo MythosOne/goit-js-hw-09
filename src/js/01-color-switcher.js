abef32ba70a1887b5128c5f7921beaa23fbdbc44
MythosOne <107143884+MythosOne@users.noreply.github.com>
2022-11-13 19:47:26 +0200

    goit-js-hw-09-1,3-correct

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonStart = document.querySelector('.data-start');
-const buttonStop = document.querySelector('.data-stop');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
 // console.log(buttonStart);
 // console.log(buttonStop);
@@ -14,14 +14,14 @@ buttonStart.addEventListener('click', () => {
     timerId = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
-    buttonStart.disabled = true;
-    buttonStop.disabled = false;
+    buttonStart.setAttribute('disabled', true);
+    buttonStop.removeAttribute('disabled');
 });
 
 buttonStop.addEventListener('click', () => {
     clearInterval(timerId);
-    buttonStop.disabled = true;
-    buttonStart.disabled = false;
+    buttonStop.setAttribute('disabled', true);
+    buttonStart.removeAttribute('disabled');
 });
 
 
