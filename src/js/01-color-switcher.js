3f16f84a2b60a4c9776a986c93624f16b7cb5ed1
MythosOne <107143884+MythosOne@users.noreply.github.com>
2022-11-13 19:30:40 +0200

    goit-js-hw-09-1,3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const buttonStart = document.querySelector('.data-start');
+const buttonStop = document.querySelector('.data-stop');
+
+// console.log(buttonStart);
+// console.log(buttonStop);
+
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+});
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    buttonStop.disabled = true;
+    buttonStart.disabled = false;
+});
+
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay }); 
+      }
+    }, delay);
+  });
+  }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+  function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { delay, step, amount }
+    } = event.currentTarget;
+  
+    const dataUser = {
+      delay: delay.value,
+      step: step.value,
+      amount: amount.value
+    };
+    let delayTime = Number(dataUser.delay);
+    let stepTime=Number(dataUser.step);
+    // console.log(dataUser);
+  event.currentTarget.reset();
+  
+  for (let i = 1; i <= Number(dataUser.amount); i++){
+    // console.log(Number(dataUser.amount));
+    createPromise(i, delayTime)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayTime += stepTime;
   }
 }
